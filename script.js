897fb59d6a16517fa5d3f76399b4f0b2fd708c12
indre101 <zygaityte.indre@gmail.com>
2019-03-29 11:27:03 +0100

    added new script and html tag

--- script.js
+++ script.js
@@ -3,28 +3,33 @@ const btn1 = document.querySelector("button");
 const msg = document.getElementById("msg");
 
 /// computer value
-function randomComputerNumber() {
 
-  let computerNumber = Math.floor(Math.random() * 100);
-  return computerNumber;
+// let computerNumber = Math.floor(Math.random() * 100);
+let computerNumber = 2
 
-}
+
 
 // assigning user value
 
 btn1.onclick = function () {
 
   let userValue;
-
-
   if (input.value < 0 || input.value > 100) {
     msg.textContent = "Please write number between 0 and a 100";
     input.value = "";
 
-
-  } else if () {
+  } else if (input.value < computerNumber) {
     userValue = input.value
+    msg.textContent = `Your guess was ${userValue} and Computer's guess was ${computerNumber}, so guess higher`;
+    input.value = "";
 
+  } else if (input.value > computerNumber) {
+    userValue = input.value
+    msg.textContent = `Your guess was ${userValue} and Computer's guess was ${computerNumber}, so guess lower`;
+    input.value = "";
+  } else {
+    userValue = input.value
+    msg.textContent = `You think same as computer You both guessed ${computerNumber}`;
+    input.value = "";
   }
-  console.log(userValue)
 }
