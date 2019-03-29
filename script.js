2dc2241441a3abdadc78b6d37d07b7cec9fca60c
indre101 <zygaityte.indre@gmail.com>
2019-03-29 11:45:33 +0100

    functionality added

--- script.js
+++ script.js
@@ -3,31 +3,53 @@ const btn1 = document.querySelector("button");
 const msg = document.getElementById("msg");
 
 /// computer value
+const computerValue = () => {
+  let computerNumber = Math.floor(Math.random() * 100);
+  return computerNumber
+}
 
-let computerNumber = Math.floor(Math.random() * 100);
+document.addEventListener("keydown", keyStroked);
 
 
-// assigning user value
 
 btn1.onclick = function () {
+  computerValue();
+  valueCompare()
+}
+
+
+function keyStroked(e) {
+  switch (e.which) {
+    case 13:
+      valueCompare();
+      break;
+    case 32:
+      valueCompare();
+      break;
+
+  }
+}
+//
+
+function valueCompare() {
 
   let userValue;
   if (input.value < 0 || input.value > 100) {
     msg.textContent = "Please write number between 0 and a 100";
     input.value = "";
 
-  } else if (input.value < computerNumber) {
+  } else if (input.value < computerValue()) {
     userValue = input.value
-    msg.textContent = `Your guess was ${userValue} and Computer's guess was ${computerNumber}, so guess higher`;
+    msg.textContent = `Your guess was ${userValue} and Computer's guess was ${computerValue()}, so guess higher`;
     input.value = "";
 
-  } else if (input.value > computerNumber) {
+  } else if (input.value > computerValue()) {
     userValue = input.value
-    msg.textContent = `Your guess was ${userValue} and Computer's guess was ${computerNumber}, so guess lower`;
+    msg.textContent = `Your guess was ${userValue} and Computer's guess was ${computerValue()}, so guess lower`;
     input.value = "";
   } else {
     userValue = input.value
-    msg.textContent = `You think same as computer You both guessed ${computerNumber}`;
+    msg.textContent = `You think same as computer You both guessed ${computerValue()}`;
     input.value = "";
   }
 }
